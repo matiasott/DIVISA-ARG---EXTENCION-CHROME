2c0aa5af4c09a83ebcbb4317bfa1f85e03c33a46
matiasott <jorgematiasott@gmail.com>
2023-06-28 01:55:22 -0300

    calculadora funcionando

--- calculadora.js
+++ calculadora.js
@@ -21,18 +21,14 @@ function calcular() {
     var cantidad = parseFloat(document.getElementById('cantidad').value);
     var opcion = document.getElementById('opcion').value;
 
-    if (tipo == "Peso Argentino"){
-
-        calcularEquivalenciaPeso(cantidad, opcion)
-
-        
-    }else{
-
-        var equivalencia = calcularEquivalencia(tipo, cantidad, opcion);
-        var simbolofinal = simbolo(tipo)
-        document.getElementById('resultado').innerHTML = 'Equivalente a <strong> ' + equivalencia + " "+ simbolofinal + " </strong>a precio de " + opcion + " en " + tipo;
-
+    if (isNaN(cantidad) || cantidad === 0) {
+        alert('Por favor, ingresa un valor numérico válido.');
+        return; // Detener la ejecución de la función
     }
+
+    var equivalencia = calcularEquivalencia(tipo, cantidad, opcion);
+    var simbolofinal = simbolo(tipo)
+    document.getElementById('resultado').innerHTML = 'Equivalente a <strong> ' + equivalencia + " " + simbolofinal + " </strong>a precio de " + opcion + " en " + tipo;
 
 }
 
@@ -76,21 +72,61 @@ function simbolo(valor) {
 }
 
 
-function calcularEquivalenciaPeso(cantidad, opcion) {
+function mostrarAR() {
+    document.getElementById("casillaAR").style.display = "block";
+    document.getElementById("casillaUS").style.display = "none";
+}
+
+function mostrarUS() {
+    document.getElementById("casillaUS").style.display = "block";
+    document.getElementById("casillaAR").style.display = "none";
+}
+
+document.getElementById("buttonAR").addEventListener("click", function () {
+    mostrarAR();
+});
+
+document.getElementById("buttonUS").addEventListener("click", function () {
+    mostrarUS();
+});
+
+document.getElementById("botonCalcular2").addEventListener("click", function () {
+    calcular2();
+});
+
+
+function calcular2(){
+
+
+    var tipoCambiar = document.getElementById('tipo2').value;
+    var cantidadValor = parseFloat(document.getElementById('cantidad2').value);
+    var opcionPrecio = document.getElementById('opcion2').value;
+
+    if (isNaN(cantidadValor) || cantidadValor === 0) {
+        alert('Por favor, ingresa un valor numérico válido.');
+        return; // Detener la ejecución de la función
+    }
+
+    var resultadoUS=calcularEquivalenciaPeso(tipoCambiar,cantidadValor, opcionPrecio)
+
+    document.getElementById('resultado2').innerHTML = 'Equivalente a <strong> ' + resultadoUS + " AR$ </strong>usando el valor " + opcionPrecio + " del " + tipoCambiar
+
+
+
+}
+
+function calcularEquivalenciaPeso(tipo,cantidad,opcion) {
+
+    console.log(tipo)
+    console.log(cantidad)
+    console.log(opcion)
+
     var diccionarioValores = obtenerDiccionarioDeStore();
-    var arrayDatos=[]
-    diccionarioValores[""].opcion
-
-
-
-    
-
-
-    
-
-
-    var resultado = cantidad / valor;
+    console.log(diccionarioValores)
+    var valor = diccionarioValores[tipo][opcion]
+    console.log(valor)
+    var resultado = cantidad * valor;
     return resultado.toFixed(2); // Redondear el resultado a 2 decimales
 
-    
+
 }
